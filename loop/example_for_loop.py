76b5c80d7686ca138ce5b0a5bac77013f343af1d
Kemoy Campbell <kscics@rit.edu>
2024-10-18 09:05:31 -0400

    example of using python for loop

---
+++ loop/example_for_loop.py
@@ -0,0 +1,28 @@
+#without putting down the start, the default start value will be 0
+print("stop")
+for i in range(10):
+    print(i)
+
+#we can also define where to start
+print("start,stop")
+for i in range(1,10):
+    print(i)
+
+#using the kth value
+print("using kth")
+for i in range(1,10,2):
+    print(i)
+
+print("The length")
+print(len(range(1,3)))
+
+#print from 1 to 5
+"""
+for num = 1 to 5
+    print(num)
+end for
+"""
+print("number 1 to 5")
+for num in range(5):
+    #we add 1 because of the zero based counting
+    print(num+1)
